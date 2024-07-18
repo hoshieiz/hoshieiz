b824c16bb302405b31d3727f4d8be9d4db1e7f6d
hoshieiz <nureizrinsoffea@gmail.com>
2024-07-18 22:35:27 +0800

    Update app.js

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ class App{
 		this.scene = new THREE.Scene();
         this.scene.add( this.dolly );
         
-		const ambient = new THREE.HemisphereLight(0xFFFFFF, 0xAAAAAA, 0.8);
+		const ambient = new THREE.HemisphereLight(0xFFFF00, 0x000000, 0.3);
 		this.scene.add(ambient);
 
 		this.renderer = new THREE.WebGLRenderer({ antialias: true });
